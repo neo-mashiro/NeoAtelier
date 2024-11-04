e265ad9f323df67cbfc935bfd74c1e3e5ae0338f
neo-mashiro <neo-mashiro@hotmail.com>
2024-11-03 22:39:39 -0800

    update site url

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,5 +1,8 @@
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && (pwd -W 2> /dev/null || pwd))"
 source "${THIS_DIR}/../setup.sh"
 
+activate_venv
+
 cd ${ATELIER_DOCS_ROOT}
+print_info "Deploying documentation..."
 mkdocs gh-deploy --no-history
